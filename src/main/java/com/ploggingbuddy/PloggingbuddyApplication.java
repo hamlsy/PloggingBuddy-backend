f73d1ff1ea20c4494bb9564a77149bbbe8a9a93b
hamlsy <dltmddud1122@naver.com>
2025-05-15 01:00:36 +0900

    init

---
+++ src/main/java/com/ploggingbuddy/PloggingbuddyApplication.java
@@ -0,0 +1,13 @@
+package com.ploggingbuddy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PloggingbuddyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PloggingbuddyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ploggingbuddy/PloggingbuddyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ploggingbuddy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PloggingbuddyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
