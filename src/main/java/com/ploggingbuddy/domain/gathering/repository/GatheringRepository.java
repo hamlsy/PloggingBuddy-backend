9cb0661516e7402386fcd4c6874bda67b999f170
Choi Min Young <codingmy@naver.com>
2025-05-17 02:16:29 +0900

    refactor: remove unnecessary overrides of JpaRepository methods #5

--- src/main/java/com/ploggingbuddy/domain/gathering/repository/GatheringRepository.java
+++ src/main/java/com/ploggingbuddy/domain/gathering/repository/GatheringRepository.java
@@ -6,7 +6,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.Optional;
 
 public interface GatheringRepository extends JpaRepository<Gathering, Long> {
-    void saveGathering(Gathering gathering);
 
-    Optional<Gathering> findById(Long id);
 }
