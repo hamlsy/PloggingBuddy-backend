4eaa6602f4c8d5efab8ff7379a2d1dcac7bb3e20
Choi Min Young <codingmy@naver.com>
2025-05-17 03:40:45 +0900

    docs: swagger setting #5

--- src/main/java/com/ploggingbuddy/presentation/gathering/controller/GatheringController.java
+++ src/main/java/com/ploggingbuddy/presentation/gathering/controller/GatheringController.java
@@ -8,6 +8,8 @@ import com.ploggingbuddy.presentation.gathering.dto.request.PostGatheringPostDto
 import com.ploggingbuddy.presentation.gathering.dto.request.UpdateGatheringAmountDto;
 import com.ploggingbuddy.presentation.gathering.dto.request.UpdatePostStatusAsDeletedDto;
 import com.ploggingbuddy.security.aop.CurrentMember;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -18,12 +20,14 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/gathering")
 @RequiredArgsConstructor
+@Tag(name = "Gathering Api", description = "게시글 API")
 public class GatheringController {
     private final CreateGatheringUseCase createGatheringUseCase;
     private final UpdatePostStatusAsDeletedUseCase updatePostStatusAsDeletedUseCase;
     private final UpdateGatheringAmountUseCase updateGatheringAmountUseCase;
 
     @PostMapping("/new")
+    @Operation(summary = "모집 게시글 작성", description = "새 모집 게시글을 작성하는 api입니다.")
     public ResponseEntity<Void> postNewGathering(
             @CurrentMember Member member,
             @RequestBody PostGatheringPostDto requestBody
@@ -33,6 +37,7 @@ public class GatheringController {
     }
 
     @PostMapping("/delete")
+    @Operation(summary = "모집 게시글 삭제", description = "본인이 작성한 게시글을 삭제하는 api입니다.")
     public ResponseEntity<Void> updateGatheringAsDeleted(
             @CurrentMember Member member,
             @RequestBody UpdatePostStatusAsDeletedDto requestBody
@@ -42,6 +47,7 @@ public class GatheringController {
     }
 
     @PostMapping("/update")
+    @Operation(summary = "모집 게시글 인원 수정", description = "본인이 작성한 게시글의 모집 인원을 수정하는 api입니다.")
     public ResponseEntity<Void> updateGatheringAmount(
             @CurrentMember Member member,
             @RequestBody UpdateGatheringAmountDto requestBody
