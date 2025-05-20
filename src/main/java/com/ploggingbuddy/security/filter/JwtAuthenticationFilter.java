bca59d8a8d72b375cc2225fc25a111abe3862993
hamlsy <dltmddud1122@naver.com>
2025-05-20 15:53:04 +0900

    fix: resolve jwt filter error #31

--- src/main/java/com/ploggingbuddy/security/filter/JwtAuthenticationFilter.java
+++ src/main/java/com/ploggingbuddy/security/filter/JwtAuthenticationFilter.java
@@ -51,10 +51,12 @@ public class JwtAuthenticationFilter extends OncePerRequestFilter {
 
     private String resolveToken(HttpServletRequest request) {
         String bearerToken = request.getHeader("Authorization");
-
-        if (StringUtils.hasText(bearerToken) && bearerToken.startsWith("Bearer")) {
+        if (!StringUtils.hasText(bearerToken)) {
+            return null;
+        }
+        if (StringUtils.hasText(bearerToken) && bearerToken.startsWith("Bearer ")) {
             return bearerToken.substring(7);
         }
-        return null;
+        return bearerToken;
     }
 }
